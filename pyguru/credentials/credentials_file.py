9e9191d94d956103c4c70245cbf8be6f95891b8b
RonMordoch <mordoch.ron@gmail.com>
2025-02-10 13:27:43 +0000

    - CredentialsFile: `write` method - TestCredentialsFile: test for writing

--- pyguru/credentials/credentials_file.py
+++ pyguru/credentials/credentials_file.py
@@ -16,3 +16,22 @@ class CredentialsFile:
         config = configparser.ConfigParser()
         config.read(filepath)
         return tuple(config.get(profile_name, key) for key in [cls.UNAME_KEY, cls.PWD_KEY])
+
+    @classmethod
+    def write(cls, username: str, password: str, profile_name: str = None, filepath: Path = None):
+        """
+        Writes the given credentials into the specified file path, appending the data if file exists.
+        If profile name exists, it will overwrite the existing credentials with the given ones.
+        """
+        filepath = filepath or cls.CREDENTIALS_FILEPATH
+        profile_name = profile_name or cls.DEFAULT_PROFILE_NAME
+        config = configparser.ConfigParser()
+        if filepath.exists():
+            config.read(filepath)
+        config[profile_name] = {
+            CredentialsFile.UNAME_KEY: username,
+            CredentialsFile.PWD_KEY: password
+        }
+
+        with filepath.open('w') as f:
+            config.write(f)
--- tests/credentials/test_credentials_file.py
+++ tests/credentials/test_credentials_file.py
@@ -1,6 +1,16 @@
+import configparser
+from pathlib import Path
+
+import pytest
+
 from pyguru.credentials.credentials_file import CredentialsFile
 from tests.credentials.test_credentials import (TestCredentials,
                                                 mock_credentials_file)
+
+
+@pytest.fixture
+def credentials_tmp_filepath(tmpdir):
+    yield Path(tmpdir) / 'credentials'
 
 
 class TestCredentialsFile:
@@ -9,3 +19,14 @@ class TestCredentialsFile:
         username, password = CredentialsFile.load()
         assert username == TestCredentials.TEST_UNAME
         assert password == TestCredentials.TEST_PWD
+
+    def test_write_new_file(self, credentials_tmp_filepath):
+        CredentialsFile.write(
+            username=TestCredentials.TEST_UNAME,
+            password=TestCredentials.TEST_PWD,
+            filepath=credentials_tmp_filepath
+        )
+        config = configparser.ConfigParser()
+        config.read(credentials_tmp_filepath)
+        assert config.get(CredentialsFile.DEFAULT_PROFILE_NAME, CredentialsFile.UNAME_KEY) == TestCredentials.TEST_UNAME
+        assert config.get(CredentialsFile.DEFAULT_PROFILE_NAME, CredentialsFile.PWD_KEY) == TestCredentials.TEST_PWD
